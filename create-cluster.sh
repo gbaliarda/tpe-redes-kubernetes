074be60a9974a7625f703d478179e584256333ec
gbaliarda <gbaliarda@itba.edu.ar>
2024-05-17 21:21:26 -0300

    feat: nginx server

--- create-cluster.sh
+++ create-cluster.sh
@@ -9,3 +9,6 @@ kind load docker-image apiexpress:latest --name redes-cluster
 cd ..
 kubectl apply -f ./k8s/database/
 kubectl apply -f ./k8s/api/
+kubectl apply -f ./nginx/controller-nginx.yaml
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=180s && kubectl apply -f ./nginx/ingress-nginx.yaml
+kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 8080:80
